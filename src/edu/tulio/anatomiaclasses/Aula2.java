509e50e25e55656209503ced5f49da236dd75dee
bastostuliojrrt <bastostuliojrrt@gmail.com>
2024-08-13 00:05:09 -0300

    Aula 2 - Como declarar variaveis

---
+++ src/edu/tulio/anatomiaclasses/Aula2.java
@@ -0,0 +1,23 @@
+package edu.tulio.anatomiaclasses;
+
+public class Aula2 {
+
+    public static void main(String[] args) {
+
+        /*
+        ° Deve conter apenas Letras, Underline, $ ou os números de 0 a 9
+        ° Deve obrigatoriamente se iniciar por uma letra(preferencialmente), ou Underline ou $, mas nunca com número
+        ° Deve iniciar com letra minúscula
+        ° Não pode conter espaços
+        ° Não podemos usar palavras-chave da linguegem
+        ° O nome deve ser único dentro de um escopo
+         */
+
+        final String BR = "Brasil"; // Definindo uma variável constante
+        double PI = 3.14;
+        final int ESTADOS_BRASILEIRO = 27;
+
+
+    }
+
+}
